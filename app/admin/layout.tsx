3c5d74b9f587dc6a2aa964c0b05bc00d3006f877
rakibislam2233 <rakib2020.tkg@gmail.com>
2026-02-28 21:50:53 +0600

    feat: implement initial admin dashboard UI including layout, sidebar, header, and dashboard content.

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,22 @@
+import AdminHeader from "@/components/Pages/Admin/AdminHeader";
+import Sidebar from "@/components/Pages/Admin/Sidebar";
+import { Metadata } from "next";
 import React from "react";
 
+export const metadata: Metadata = {
+  title: "Admin Dashboard | QuickHire",
+};
+
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main className="w-full min-h-screen">{children}</main>;
+  return (
+    <div className="flex min-h-screen bg-white">
+      <Sidebar />
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+        <AdminHeader />
+        <main className="flex-1 overflow-y-auto bg-white p-8">{children}</main>
+      </div>
+    </div>
+  );
 };
 
 export default AdminLayout;
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import DashboardContent from "@/components/Pages/Admin/DashboardContent";
+import { Metadata } from "next";
 
-const AdminPage = () => {
-  return (
-    <div>AdminPage</div>
-  )
+export const metadata: Metadata = {
+  title: "Admin Dashboard | QuickHire",
+};
+
+export default function AdminPage() {
+  return <DashboardContent />;
 }
-
-export default AdminPage
