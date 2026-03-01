f4c3a2d0154143fcd5d9e4fad7f613bffe75d84d
Md Rakib Ali <rakib2020.tkg@gmail.com>
2026-03-01 10:12:44 +0600

    feat: add HeroSection and ExploreCategories components for the home page.

--- components/Pages/Main/Home/ExploreCategories.tsx
+++ components/Pages/Main/Home/ExploreCategories.tsx
@@ -1,21 +1,20 @@
 import {
   ArrowRight,
   BarChart,
-  Briefcase,
-  Code,
-  Megaphone,
   Monitor,
-  PenTool,
-  Users,
-  Wallet,
 } from "lucide-react";
 import Link from "next/link";
+import { HiOutlineBriefcase } from "react-icons/hi";
+import { HiOutlineMegaphone } from "react-icons/hi2";
+import { IoCodeSlash } from "react-icons/io5";
+import { LuPencilRuler } from "react-icons/lu";
+import { PiMoneyLight, PiUsersThree } from "react-icons/pi";
 
 const categories = [
   {
     title: "Design",
     jobs: 235,
-    icon: PenTool,
+    icon: LuPencilRuler,
     isActive: false,
   },
   {
@@ -27,13 +26,13 @@ const categories = [
   {
     title: "Marketing",
     jobs: 140,
-    icon: Megaphone,
+    icon: HiOutlineMegaphone,
     isActive: true,
   },
   {
     title: "Finance",
     jobs: 325,
-    icon: Wallet,
+    icon: PiMoneyLight,
     isActive: false,
   },
   {
@@ -45,19 +44,19 @@ const categories = [
   {
     title: "Engineering",
     jobs: 542,
-    icon: Code,
+    icon: IoCodeSlash,
     isActive: false,
   },
   {
     title: "Business",
     jobs: 211,
-    icon: Briefcase,
+    icon: HiOutlineBriefcase,
     isActive: false,
   },
   {
     title: "Human Resource",
     jobs: 346,
-    icon: Users,
+    icon: PiUsersThree,
     isActive: false,
   },
 ];
@@ -68,7 +67,7 @@ const ExploreCategories = () => {
       <div className="w-full container mx-auto px-5 md:px-16">
         {/* Header */}
         <div className="flex flex-col md:flex-row md:items-center justify-between gap-4 mb-12">
-          <h2 className="text-4xl md:text-5xl font-bold text-[#25324B]">
+          <h2 className="text-4xl md:text-5xl font-semibold text-[#25324B]">
             Explore by <span className="text-[#26A4FF]">category</span>
           </h2>
           <Link
@@ -81,7 +80,7 @@ const ExploreCategories = () => {
         </div>
 
         {/* Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
           {categories.map((category) => (
             <Link
               href={`/jobs?category=${category.title.toLowerCase()}`}
@@ -89,7 +88,7 @@ const ExploreCategories = () => {
               className={`group flex items-start gap-0 p-8 pt-10 border transition-all duration-300 ${
                 category.isActive
                   ? "bg-primary border-primary text-white shadow-xl hover:-translate-y-1"
-                  : "bg-white border-gray-100/60 hover:border-primary/20 hover:shadow-lg hover:-translate-y-1"
+                  : "bg-white border-[#D6DDEB] hover:shadow-lg hover:-translate-y-1 "
               }`}
             >
               <div className="flex flex-col w-full h-full">
@@ -101,7 +100,7 @@ const ExploreCategories = () => {
                   <category.icon strokeWidth={1.5} className="w-10 h-10" />
                 </div>
                 <h3
-                  className={`text-xl font-bold mb-3 font-sans ${
+                  className={`text-xl font-semibold mb-3 font-sans ${
                     category.isActive ? "text-white" : "text-[#25324B]"
                   }`}
                 >
